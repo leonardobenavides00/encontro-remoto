45b88f68f8c0bc2a9ef476de0774edf879e753e9
leonardobenavides <leonardobenavides2@gmail.com>
2022-06-21 13:05:01 -0500

    Adiciona a atividade do encontro remoto 2

---
+++ encontroRemoto2.js
@@ -0,0 +1,12 @@
+let alunosPresentes=13
+for (let aluno=0;aluno<=alunosPresentes;aluno++){
+    if(aluno==0){
+        console.log("Zero "+aluno);
+        }else {
+            if (aluno%2==0){
+                console.log("Par "+aluno);
+            } else {
+                console.log("Impar "+aluno);
+            }
+        }
+}
